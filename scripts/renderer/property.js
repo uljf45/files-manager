05a6ac3842c99b1216d69d0b4ff4488c8488bbd6
uljf45 <uljf45@163.com>
2020-06-18 18:04:47 +0800

    add show file property 2

--- scripts/renderer/property.js
+++ scripts/renderer/property.js
@@ -1,10 +1,52 @@
 const {ipcRenderer, remote} = require('electron')
 const db = require('../datastore')
+const util = require('../../scripts/web/util')
+const path = require('path')
+const fs = require('fs-extra')
 
 // ipcRenderer.on('show', (event, args) => {
 //     let notify = new Notification('Name Exists!')
 // })
-function test() {
+
+let template = {
+    property: `
+        <div class="property-wrap">
+            <span class="property-name">$0</span>
+            <span class="property-content">$1</span>
+        </div>
+    `,
+    separator: `<div class="property-separator"></div>`
+}
+
+let proertyListDom = document.querySelector('.property-list')
+
+function makePropertyDom (params) {
+    let html 
+
+    switch(util.typeof(params)) {
+        case 'Object':
+            if (param.type == 'separator') {
+                return template.separator
+            }
+            html = template.property.replace('$1', params.content).replace('$0', params.name)
+            return util.createElement(html)
+        break
+        case 'Array':
+            html = params.map(param => {
+                if (param.type == 'separator') {
+                    return template.separator
+                }
+                return template.property.replace('$1', param.content).replace('$0', param.name)    
+            }).join('')
+            return util.createElements(html)
+
+        break
+    }
+    
+    
+}
+
+function init() {
     remote.getCurrentWindow().getParentWindow().webContents.send('propertyId', {
         id: remote.getCurrentWindow().id
     })
@@ -15,9 +57,46 @@ function test() {
             let file = db.read().get('files').find({id}).value()
             if (file) {
                 document.title = file.name + ' Property'
+                let isExist = fs.existsSync(file.path)
+                if (isExist) {
+                    let stat = fs.statSync(file.path)
+                    if (fs.link)
+                    console.log(stat)
+                    proertyListDom.append(...makePropertyDom([{
+                        name: '名称:',
+                        content: path.basename(file.path)
+                    }, {
+                        name: '位置:',
+                        content: path.dirname(file.path)
+                    }, {
+                        name: '大小:',
+                        content: `${util.bytesFormat(stat.size, 'GB')} (${util.toThousands(stat.size)} 字节)`
+                    }, {
+                        type: 'separator'
+                    }, {
+                        name: '创建时间:',
+                        content: stat.birthtime.toLocaleString()
+                    }, {
+                        name: '修改时间:',
+                        content: stat.ctime.toLocaleString()
+                    }, {
+                        name: '访问时间:',
+                        content: stat.atime.toLocaleString()
+                    }, {
+                        name: 'mode:',
+                        content: stat.mode
+                    }]))
+                } else {
+                    proertyListDom.append('Not exists!')
+                }
+                
             }
         }
 
     })
+
+    document.querySelector('#property-cancel').addEventListener('click', function (e) {
+        remote.getCurrentWindow().close()
+    })
 }
-test()
+init()
--- scripts/web/util.js
+++ scripts/web/util.js
@@ -59,4 +59,44 @@ util.debounce = function (func, wait, immediate) {
     }
 }
 
+util.createElement = function (html) {
+    let div = document.createElement('div')
+    div.innerHTML = html
+    return div.children[0]
+}
+
+util.createElements = function (html) {
+    let div = document.createElement('div')
+    div.innerHTML = html
+    return Array.prototype.slice.call(div.children)
+}
+
+
+util.toThousands =  function (num) {
+    return (num || 0).toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,');
+}
+
+util.typeof = function(entity) {
+    let matchStr = Object.prototype.toString.call(entity).match(/^\[object (\w*)\]$/)
+    return matchStr && matchStr[1]
+}
+
+util.bytesFormat = function(bytes, unit) {
+    bytes = Number(bytes)
+    let units = ["B", "KB", "MB", "GB", "TB"]
+    if (unit == null) {
+        let n = 0 
+        while(bytes >= 1024) {
+            n++
+            bytes = bytes / 1024
+        }
+        return `${bytes.toFixed(2)} ${units[n]}`
+    }
+    let idx = units.indexOf(unit)
+    if (idx == null) return
+    for (let i = 1; i <= idx; i++) bytes = (bytes / 1024)
+    return `${bytes.toFixed((idx-1) * 3 + 3)} ${unit}`
+    
+}
+
 module.exports = util
